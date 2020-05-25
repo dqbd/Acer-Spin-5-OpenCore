ffb2e91737824d2ec258641cc7c1a6845c1c29ba
Duong Tat Dat <david@duong.cz>
2020-05-25 18:16:13 +0200

    Use D letter

--- EFI/WIP/acpi-copy.ps1
+++ EFI/WIP/acpi-copy.ps1
@@ -1,4 +1,4 @@
-Remove-Item "E:\EFI\OC\ACPI" -Confirm:$false -Recurse -Verbose
-Remove-Item "E:\EFI\OC\config.plist" -Confirm:$false -Verbose
-Copy-Item ..\OC\ACPI -Destination "E:\EFI\OC" -Recurse -Verbose
-Copy-Item ..\OC\config.plist -Destination "E:\EFI\OC" -Verbose
+Remove-Item "D:\EFI\OC\ACPI" -Confirm:$false -Recurse -Verbose
+Remove-Item "D:\EFI\OC\config.plist" -Confirm:$false -Verbose
+Copy-Item ..\OC\ACPI -Destination "D:\EFI\OC" -Recurse -Verbose
+Copy-Item ..\OC\config.plist -Destination "D:\EFI\OC" -Verbose
